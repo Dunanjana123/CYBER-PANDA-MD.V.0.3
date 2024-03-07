bc396f264fc19c71b17a0e844de4be41cff044ad
Dunanjana123 <162582111+Dunanjana123@users.noreply.github.com>
2024-03-07 18:00:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,16 +7,16 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94713106474', '𝘚𝘈𝘊𝘏𝘐𝘛𝘏', true],
-  ['94762537450', '𝘊𝘠𝘉𝘌𝘙-𝘗𝘈𝘕𝘋𝘈', true], 
+  ['94769493519', 'Dunanjana', true],
+  ['947669493519', 'Lanka Beat Music', true], 
   [''] 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['+94704274987','+94704274987'] 
-global.prems = ['+94704274987', '+94704274987', '+94704274987']
-global.allowed = ['+94704274987', '+94704274987', '+94704274987']
+global.mods = ['+94769493519','+94769493519'] 
+global.prems = ['+94769493519', '+94769493519', '+94769493519']
+global.allowed = ['+94769493519', '+94769493519', '+94769493519']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -49,16 +49,16 @@ global.APIKeys = { // APIKey Here
   'https://api-fgmods.ddns.net': 'fg-dylux'
 }
 
-global.botname = '🦋 │𝐂𝐘𝐁𝜩𝐑│𝐏𝜟𝐍𝐃𝐀│𝐌𝐃│𝐕➂ 🦋'
+global.botname = '🎶 Dunanjana Nethmina 🎶'
 global.premium = 'true'
-global.packname = '🦋 │𝐂𝐘𝐁𝜩𝐑│𝐏𝜟𝐍𝐃𝐀│𝐌𝐃│𝐕➂ 🦋' 
-global.author = '@SACHITH' 
-global.menuvid = 'https://telegra.ph/Hi-12-11-189'
+global.packname = '🎶 Dunanjana Nethmina 🎶' 
+global.author = '@DUNANJANA' 
+global.menuvid = 'https://www.youtube.com/channel/UC1LWweyMXPhNoRLzrzJNVsA'
 global.igfg = '\n\n' 
 global.dygp = ''
 global.fgsc = 'https://github.com/CYBER-x-SACHIYA-SL-MD-BOT/CYBER-PANDA-MD.V.0.3' 
-global.fgyt = 'https://youtube.com/@senuwamod'
-global.fgpyp = 'https://youtube.com/@senuwamod'
+global.fgyt = 'https://www.youtube.com/channel/UC1LWweyMXPhNoRLzrzJNVsA'
+global.fgpyp = 'https://www.youtube.com/channel/UC1LWweyMXPhNoRLzrzJNVsA'
 global.fglog = 'https://raw.githubusercontent.com/black/api/Guru/guru.jpg' 
 global.thumb = fs.readFileSync('./Assets/Gurulogo.jpg')
 
